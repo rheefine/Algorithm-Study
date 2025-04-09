8233ac215d49ec46cecd3a98ed9c217cd6c86f7a
Rhee <rhee@Rheeui-MacBookAir.local>
2025-04-09 13:08:16 +0900

    0x03: Array refactor

--- 0x03_array/ex02.py
+++ 0x03_array/ex02.py
@@ -1,12 +1,10 @@
-# 시간 복잡도: O(n)
 def solution1(arr, n):
-    num = [0] * 101
-    for i in range(n):
-        num[arr[i]] = 1 
-    for i in range(101):
-        if num[i] == 1 and num[100-i] == 1 and i != 100-i:
+    cnt = [0] * 101
+    for i in arr:
+        if cnt[100 - i] == 1:
             return 1
-    return 0
+        cnt[i] = 1
+    return 0            
 
 if __name__ == '__main__':
     arr = [1,52,48]
